58ae3f375a6fe245bef19772845f98590f597745
grufkork <griffin.hiscoke@me.com>
2024-03-25 18:54:39 +0100

    first proper working version

--- outfox-lights/pad/pad.ino
+++ outfox-lights/pad/pad.ino
@@ -13,42 +13,43 @@ bool key[] = {false, false, false, false};
 
 int cooldown[] = {0, 0, 0, 0};
 
-float avgs[] = {512.0f, 512.0f, 512.0f, 512.0f};
+float avgs[] = {0.0f, 0.0f, 0.0f, 0.0f};
 
 int val;
 
 void loop() {
-  //val = analogRead(A2);
+  //val = analogRead(A1);
   //avgs[0] = (avgs[0]+val)/2.0f;
-  //Serial.println(avgs[0]);
+  //Serial.println(val);
   //return;
 
   for(int i = 0; i < 4; i++){
     val = analogRead(pins[i]);
 
-    avgs[i] = (avgs[i]+val*10.0f)/11.0f;
+    avgs[i] = (avgs[i]+val-509)*0.99f;
 
     /*if(cooldown[i] > 0){
       cooldown[i]--;
       continue;
     }*/
 
-    if(avgs[i] >= 1000 && !key[i]){
+    if(avgs[i] >= 3000.0f && !key[i]){
+      key[i] = true;
+      Serial.write(0b10000000 + i);
+      Serial.flush();
+      /*
       if(cooldown[i] > 5){
-        key[i] = true;
-        Serial.write(0b10000000 + i);
-        Serial.flush();
       }else{
         cooldown[i] ++;
-      }
-    }else if(avgs[i] <= 20 && key[i]){
-      if(cooldown[i] > 5){
-        key[i] = false;
-        Serial.write(0b00000000 + i);
-        Serial.flush();
+      }*/
+    }else if(avgs[i] <= -4000.0f && key[i]){
+      key[i] = false;
+      Serial.write(0b00000000 + i);
+      Serial.flush();
+      /*if(cooldown[i] > 5){
       }else{
         cooldown[i] ++;
-      }
+      }*/
     }else{
       cooldown[i]=0;
     }
