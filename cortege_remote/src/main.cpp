3607d62820f08f16f9425d6ff5c24d2d7b5b7850
gurraba <gurraba@gmail.com>
2024-04-18 23:16:42 +0200

    changed pins, added deadmans switch
    new stuff

--- cortege_remote/src/main.cpp
+++ cortege_remote/src/main.cpp
@@ -10,8 +10,10 @@
 #include "ESPNowW.h"
 
 //Potentiometer joystick code
-#define MOTORA_PIN A0
-#define MOTORB_PIN A3
+#define MOTORA_PIN A5
+#define MOTORB_PIN A4
+#define DEAD_MAN_PIN A6
+
 
 uint8_t receiver_mac[] = {0x84,0xCC,0xA8,0x60,0x6F,0xDC};
 
@@ -35,23 +37,40 @@ void setup() {
 
     pinMode(MOTORA_PIN, INPUT);
     pinMode(MOTORB_PIN, INPUT);
+    pinMode(DEAD_MAN_PIN, INPUT);
 
 }
 
 void loop() {
     //read analog values and set them to the data struct
     
-    data.motorA = map(analogRead(MOTORA_PIN), 0, 4095, 0, 127);
-    data.motorB = map(analogRead(MOTORB_PIN), 0, 4095, 0, 127);
-    data.forward = 1;
-    // map and constrain the values to -127 to 127
-    data.motorA = constrain(data.motorA, 0, 127);
-    data.motorB = constrain(data.motorB, 0, 127);
+    int buttonPressed = 0;
+    for(int i = 0; i < 5; i++){
+        buttonPressed += analogRead(DEAD_MAN_PIN) / 5;
+    }
+
+    if(buttonPressed > 3200){
+
+        
+        data.motorA = map(analogRead(MOTORA_PIN), 0, 4095, 0, 127);
+        data.motorB = map(analogRead(MOTORB_PIN), 0, 4095, 0, 127);
+        data.forward = 1;
+        // map and constrain the values to -127 to 127
+        data.motorA = constrain(data.motorA, 0, 127);
+        data.motorB = constrain(data.motorB, 0, 127);
+    }
+    else{
+        data.motorA = 0;
+        data.motorB = 0;
+    }
+  
+    Serial.println(data.motorA);
+    Serial.println(data.motorB);
 
     uint8_t *dataBytes = (uint8_t *)&data;
     int dataSize = sizeof(data);
 
-    delay(100);
+    delay(10);
     ESPNow.send_message(receiver_mac, dataBytes, dataSize);
     
 
