da9ff55fd8bbb6bb23279e4772a44c1552d32c29
grufkork <griffin.hiscoke@me.com>
2025-03-03 09:58:14 +0100

    Graceful error handling

--- outfox-lights/src/main.rs
+++ outfox-lights/src/main.rs
@@ -1,14 +1,13 @@
-
 use std::str::FromStr;
 use std::sync::mpsc::channel;
+use std::sync::mpsc::{Sender, Receiver};
 use std::thread;
 use std::{thread::sleep, time::Duration};
 use std::net::{UdpSocket, SocketAddr};
 use std::env;
 
-use serialport::UsbPortInfo;
 use toy_arms::external::{read, Process};
-use inputbot::{KeySequence, KeybdKey::*, MouseButton::*};
+use inputbot::KeybdKey::*;
 
 // This program presses keys for the Arduino and fetches lights from the DDR clone
 
@@ -25,9 +24,7 @@ fn main(){
 
 
     thread::spawn(move ||{
-        let keys = vec![DownKey, RightKey, LeftKey,UpKey , EnterKey, EscapeKey];
-
-
+        
         let ports = serialport::available_ports().expect("No ports found!");
 
         println!(" -- Available ports: --");
@@ -36,67 +33,41 @@ fn main(){
         }
         println!();
 
-        // println!("{:?}", ports);
-        let portname = ports.iter().find(|x| return x.port_name.contains(&portname)
-        ).expect("No such port!!").port_name.clone();
-
-        println!("Using port {portname}");
-
-        let mut port = serialport::new(portname, 115200).timeout(Duration::from_millis(1000)).open().expect("Port open failed");
-        port.write_data_terminal_ready(true);
-
-        port.set_timeout(Duration::from_secs(999999999)).ok();
-        let mut buf = vec![0; 1];
         loop{
-            port.read_exact(&mut buf).unwrap();
-            println!("Got {:?}", buf);
-            for byte in &buf{
-                // let mut byte = *byte;
-                // if keys[(byte & 0b0111111) as usize] == LeftKey{
-                //     if byte & 0b10000000 > 0{
-                //         byte -= 0b10000000;
-                //     }else{
-                //         byte += 0b10000000;
-                //     }
-                //
-                //
-                // }
-                tx.send(*byte);
-
-                let mut val = byte & 0b0111111;
-
-                if keys[val as usize] == LeftKey{
-
-                }
-                
-
-                if (byte & 0b10000000 > 0) {
-                    println!("press {:?}", keys[val as usize]);
-                    keys[val as usize].press();
-                }else{
-                    println!("release {:?}", keys[val as usize]);
-                    keys[val as usize].release();
-
-                }
-                
-
-
-            }
+            let err = dancepad_routine(&portname, tx.clone());
+            println!("Input routine error: {err}");
+            thread::sleep(Duration::from_secs(3));
         }
+
     });
 
 
+    loop{
+        let err = outfox_routine(&rx);
 
-    let of = Process::from_process_name("OutFox.exe").unwrap();
+        println!("Outfox routine error: {err}");
+        thread::sleep(Duration::from_secs(3));
+    }
+
+    
+
+}
+
+fn outfox_routine(rx: &Receiver<u8>) -> String{
+    let Ok(of) = Process::from_process_name("OutFox.exe") else{
+        return "Outfox not found".to_string();
+    };
     let h = of.process_handle;
-    let base = of.get_module_base("OutFox.exe").unwrap();
+    let Ok(base) = of.get_module_base("OutFox.exe")else{
+        return "Outfox base not found".to_string();
+    };
     let addr = base + 0x1BE37F0;
 
 
     let address = SocketAddr::from_str("127.0.0.1:6969").unwrap();
     let socket = UdpSocket::bind("127.0.0.1:5433").unwrap();
 
-    let mut lights = vec![0; 4];
+    let mut lights = [0; 4];
 
     loop{
         while let Ok(msg) = rx.try_recv(){
@@ -105,11 +76,83 @@ fn main(){
                 lights[val as usize] = (msg & 0b10000000 > 0) as u8;
             }
         }
-        let mut vals: Vec<u8> = (0..6).map(|x| read(h, addr + x).unwrap()).collect();
-        vals.extend(lights.iter());
-        socket.send_to(&vals, address).unwrap();
+        if let Ok(mut vals) = (0..6).map(|x| read(h, addr + x)).collect::<Result<Vec<u8>, _>>(){
+            vals.extend(lights.iter());
+            socket.send_to(&vals, address).unwrap();
+        }else{
+            return "Outfox read error".to_string();
+        }
         sleep(Duration::from_millis(20));
 
     }
-
 }
+
+fn dancepad_routine(portname: &str, tx: Sender<u8>) -> String{
+
+
+    let keys = [DownKey, RightKey, LeftKey,UpKey , EnterKey, EscapeKey];
+    let Ok(ports) = serialport::available_ports() else{
+        return "No ports found".to_string();
+    };
+
+    let Some(port) = ports.iter().find(|x| x.port_name.contains(portname)) else {
+        return "Port not found".to_string();
+
+    };
+    let portname = port.port_name.clone();
+
+    println!("Using port {portname}");
+
+    let Ok(mut port) = serialport::new(portname, 115200).timeout(Duration::from_millis(1000)).open() else{
+        return "Failed to open port".to_string();
+    };
+    if port.write_data_terminal_ready(true).is_err(){
+        return "Failed to set port ready".to_string();
+    }
+
+    port.set_timeout(Duration::from_secs(9999)).ok();
+    let mut buf = vec![0; 1];
+    loop{
+        if let Err(e) = port.read_exact(&mut buf){
+            return format!("Failed to read: {e}");
+        };
+        println!("Read: {:?}", buf);
+        for byte in &buf{
+            // let mut byte = *byte;
+            // if keys[(byte & 0b0111111) as usize] == LeftKey{
+            //     if byte & 0b10000000 > 0{
+            //         byte -= 0b10000000;
+            //     }else{
+            //         byte += 0b10000000;
+            //     }
+            //
+            //
+            // }
+            if tx.send(*byte).is_err(){
+                return "Failed to send".to_string();
+            }
+
+            let val = byte & 0b0111111;
+            if val >= 6{
+                continue;
+            }
+
+
+
+            if byte & 0b10000000 > 0 {
+                print!("↓ {:?}", keys[val as usize]);
+                keys[val as usize].press();
+            }else{
+                print!("↑ {:?}", keys[val as usize]);
+                keys[val as usize].release();
+
+            }
+
+            println!("  {:0>3}", byte);
+
+
+        }
+    }
+}
+
+// !cargo r --release
